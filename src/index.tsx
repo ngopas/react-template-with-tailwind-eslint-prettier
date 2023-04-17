eed3c8c8d6b61a167205a1778269481c73cb6eeb
Fathur Rohman <fathur.rohman@kemlu.go.id>
2023-04-17 16:30:34 +0200

    minor fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,8 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import App from './App';
 const root = createRoot(document.getElementById('root') as HTMLElement);
-function App() {
-  return <h1>My React and TypeScript App!</h1>;
-}
+
 root.render(
   <StrictMode>
     <App />
