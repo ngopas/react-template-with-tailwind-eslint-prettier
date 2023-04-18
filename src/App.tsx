181eb371493cffae8bf9b121826ee631ab2ba37d
Fathur Rohman <fathur.rohman@kemlu.go.id>
2023-04-18 03:51:50 +0200

    update .gitignore add dist as ignorable

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 
+{
+  /*images*/
+}
+import Logo from './logo.svg';
+
+//const logo = require('./logo.svg') as string;
+
 const App = () => {
   return (
     <div className="container">
       <h1> Template </h1>
+      <img src={Logo} alt="Logo" className="inline-block h-20" />
     </div>
   );
 };
--- webpack.dev.config.ts
+++ webpack.dev.config.ts
@@ -1,53 +1,59 @@
 import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import {
-    Configuration as WebpackConfig,
-    HotModuleReplacementPlugin,
+  Configuration as WebpackConfig,
+  HotModuleReplacementPlugin
 } from 'webpack';
-import {
-    Configuration as WebpackDevServerConfig
-} from 'webpack-dev-server';
+import { Configuration as WebpackDevServerConfig } from 'webpack-dev-server';
 type Configuration = WebpackConfig & {
-    devServer?: WebpackDevServerConfig;
-}
+  devServer?: WebpackDevServerConfig;
+};
 
 const config: Configuration = {
-    mode: 'development',
-    output: {
-        publicPath: '/',
-    },
-    entry: './src/index.tsx',
-    module: {
-        rules: [
-            {
-                test: /\.(ts|js)x?$/i,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
-                    },
-                },
-            },
-        ],
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'src/index.html',
-        }),
-        new HotModuleReplacementPlugin(),
-    ],
-    devtool: 'inline-source-map',
-    devServer: {
-        static: path.join(__dirname, 'dist'),
-        historyApiFallback: true,
-        port: 4000,
-        open: true,
-        hot: true,
-    }
+  mode: 'development',
+  output: {
+    publicPath: '/'
+  },
+  entry: './src/index.tsx',
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react',
+              '@babel/preset-typescript'
+            ]
+          }
+        }
+      },
+      {
+        test: /\.(png|jp(e*)g|svg|gif)$/,
+        type: 'asset/resource'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+    new HotModuleReplacementPlugin()
+  ],
+  devtool: 'inline-source-map',
+  devServer: {
+    static: path.join(__dirname, 'dist'),
+    historyApiFallback: true,
+    port: 4000,
+    open: true,
+    hot: true
+  }
 };
 
 export default config;
